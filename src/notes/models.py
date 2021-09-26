5378f6e7d40e1a794556c55a7ccb6513330f80f8
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-09-26 19:07:17 +0330

    Note app and configure model

---
+++ src/notes/models.py
@@ -0,0 +1,24 @@
+from configparser import MAX_INTERPOLATION_DEPTH
+from email.policy import default
+from django.db import models
+from django.utils.timezone import now
+
+
+LABEL_CHOICES = (
+    ("DA", "dark"),
+    ("L", "light"),
+    ("D", "danger"),
+    ("W", "warning"),
+    ("SE", "secondary"),
+    ("P", "primary"),
+    ("S", "success"),
+)
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    due_date = models.DateTimeField(default=now)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=2)
+
+    def __str__(self):
+        return self.title
