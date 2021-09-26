53897d91f95062983cc51b324c8a5cfe841f1a2e
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-09-26 13:37:00 +0330

    Create a note style

--- src/notes/views.py
+++ src/notes/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Note
+from .forms import NoteForm
 
 
 def note_list_view(request):
+    form = NoteForm()
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('notes:list')
+
     undone_notes = Note.objects.filter(finished=False)
     done_notes = Note.objects.filter(finished=True)
 
     context = {
         'undone_notes': undone_notes,
         'done_notes': done_notes,
+        'form': form,
     }
 
     return render(request, 'note_list.html', context)
